f17243595a24fe8d537ed2ed6c9d8949d73e9f2f
shen2h <38775707+shen2h@users.noreply.github.com>
2025-10-25 22:33:50 +0700

    cập nhật tittle và topics title
    change theme

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: "Nulite starter for Eleventy",
-  shortTitle: "Nulite starter 💊",
+  shortTitle: "Cuộc tìm kiếm sao biển 💊",
   description:
     "A beautiful and elegant blog theme starter project por Eleventy",
   url: "https://example.com/",
